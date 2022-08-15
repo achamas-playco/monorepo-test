14a219118d92e12492b0d045478175b2217abc22
Ali Chamas <ali.chamas@play.co>
2022-08-15 21:39:46 +1000

    fix: linting

---
+++ packages/core/__test__/index.spec.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var src_1 = require("../src");
+describe('core', function () {
+    it('should be something', function () {
+        expect(src_1.Test.info).toBeTruthy();
+    });
+});
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,2 @@
 export { Test } from './test';
+// test
