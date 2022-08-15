b2c5495389bd0d61eb0e3039159cfaa16d0dbea3
Ali Chamas <ali.chamas@play.co>
2022-08-15 20:47:34 +1000

    feat: fix stuff

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -33,6 +33,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => ({
           allowSyntheticDefaultImports: true
         })
       ]
-    }
+    },
+    chunkSizeWarningLimit: 2000
   }
 }));
