3951af152982c69c490f1d1d802785199fc4569f
Ali Chamas <ali.chamas@play.co>
2022-08-15 22:08:14 +1000

    fix: lint build

--- packages/test/src/main.ts
+++ packages/test/src/main.ts
@@ -1,9 +1,10 @@
 import './theme.css';
 import './app.css';
+
 import App from './App.svelte';
 
 const app = new App({
-  target: document.getElementById('app')
+    target: document.getElementById('app'),
 });
 
 export default app;
